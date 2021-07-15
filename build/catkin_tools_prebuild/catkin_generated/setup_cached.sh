d8768bfb69e5df1da0a9bdec12432a7ef6f48a1f
Avaneen Pinninti <avaneen@gmail.com>
2021-07-15 14:25:01 -0400

    changed mission file

--- build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -8,8 +8,6 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/lesslab5/Documents/simulator_ws/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH='/opt/ros/melodic/lib'
-export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
 export PWD='/home/lesslab5/Documents/simulator_ws/build/catkin_tools_prebuild'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/lesslab5/Documents/simulator_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lesslab5/Documents/simulator_ws/devel/.private/catkin_tools_prebuild/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/lesslab5/Documents/simulator_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
