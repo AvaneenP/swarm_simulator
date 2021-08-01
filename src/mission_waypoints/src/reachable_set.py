59b97921297f2faa63128f80d34b685f84f308d9
Avaneen Pinninti <avaneen@gmail.com>
2021-08-01 17:04:08 -0400

    need to clean up last few files (boids, vel, waypoints) and also figure out how to make the reachable sets intersect more

---
+++ src/mission_waypoints/src/reachable_set.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+import rospy
+import copy
+import math
+import random
+from keyboard.msg import Key
+from std_msgs.msg import String
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import PoseStamped
+from mission_waypoints.msg import swarm_gps
+from shapely.geometry import Polygon
+
+from matplotlib import pyplot
+from shapely.geometry import LineString, Polygon
+from shapely.figures import SIZE, set_limits, plot_coords, plot_bounds, plot_line_issimple
+
+
+class ReachableSet():
+  def __init__(self):
+
+    self.uavName = rospy.get_param(str(rospy.get_name()) + "/uavName", "uav")
+    
+    self.swarm_location = swarm_gps()
+    self.curr_loc = PoseStamped()
+    self.curr_vel = Vector3()
+    self.uav_collision_name = String()
+
+    # Create the publisher and subscriber
+    self.swarm_loc_sub = rospy.Subscriber('/swarm/gps', swarm_gps, self.get_swarm_pos, queue_size = 1)
+
+    self.uav_loc = rospy.Subscriber(self.uavName + "/sensors/gps", PoseStamped, self.get_pos, queue_size = 1)
+
+    self.uav_vel = rospy.Subscriber(self.uavName + "/input/velocity", Vector3, self.get_vel, queue_size = 1)
+
+    self.uav_intersection = rospy.Publisher(self.uavName + "/intersection", String, queue_size = 1)
+
+    # Call the mainloop of our class
+    self.mainloop()
+
+  # Callback for current location
+  def get_pos(self, msg):
+    self.curr_loc.pose.position.x = msg.pose.position.x
+    self.curr_loc.pose.position.y = msg.pose.position.y
+    self.curr_loc.pose.position.z = msg.pose.position.z
+
+  def get_vel(self, msg):
+    self.curr_vel = copy.deepcopy(msg)
+
+  def get_swarm_pos(self, msg):
+    self.swarm_location = copy.deepcopy(msg)
+
+
+  def mainloop(self):
+    # Set the rate of this loop
+    rate = rospy.Rate(20)
+    rospy.sleep(10.)
+
+    # While ROS is still running
+    while not rospy.is_shutdown():        
+      
+      if self.swarm_location.name == self.uavName:
+        continue
+
+      main_uav = Polygon([ (self.curr_loc.pose.position.x + self.curr_vel.x, self.curr_loc.pose.position.y + self.curr_vel.y), (self.curr_loc.pose.position.x, self.curr_loc.pose.position.y), (self.curr_loc.pose.position.x, self.curr_loc.pose.position.y + self.curr_vel.y), (self.curr_loc.pose.position.x + self.curr_vel.x, self.curr_loc.pose.position.y) ])
+
+      swarm_uav = Polygon([ (self.swarm_location.pos.x, self.swarm_location.pos.y), (self.swarm_location.pos.x + self.swarm_location.vel.x, self.swarm_location.pos.y + self.swarm_location.vel.y), (self.swarm_location.pos.x + self.swarm_location.vel.x, self.swarm_location.pos.y), (self.swarm_location.pos.x, self.swarm_location.pos.y + self.swarm_location.vel.y) ])
+
+      if main_uav.intersects(swarm_uav):
+        self.uav_collision_name = self.swarm_location.name
+        self.uav_intersection.publish(self.uav_collision_name)
+
+      # Sleep for the remainder of the loop
+      rate.sleep()
+
+if __name__ == '__main__':
+  rospy.init_node('reachable_set_node')
+  try:
+    ktp = ReachableSet()
+  except rospy.ROSInterruptException:
+    pass
--- src/mission_waypoints/src/swarm_separation.py
+++ src/mission_waypoints/src/swarm_separation.py
@@ -4,30 +4,36 @@ import copy
 import math
 import random
 from keyboard.msg import Key
+from std_msgs.msg import String
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import PoseStamped
+from mission_waypoints.msg import swarm_gps
+from shapely.geometry import Polygon
+
+from matplotlib import pyplot
+from shapely.geometry import LineString, Polygon
+from shapely.figures import SIZE, set_limits, plot_coords, plot_bounds, plot_line_issimple
 
 class SwarmSeparation():
   def __init__(self):
+    
+    self.uavName = rospy.get_param(str(rospy.get_name()) + "/uavName", "uav")
 
-    self.curr_pos1 = PoseStamped()
-    self.curr_pos2 = PoseStamped()
-    self.curr_pos3 = PoseStamped()
-
-    self.away_vel1 = Vector3()
-    self.away_vel2 = Vector3()
-    self.away_vel3 = Vector3()
+    self.curr_pos = PoseStamped()
+    self.swarm_location = swarm_gps()
+    self.uav_intersection_name = String()
+    self.away_vel = Vector3()
 
     # Create the publisher and subscriber
-    self.position_sub1 = rospy.Subscriber('/uav1/sensors/gps', PoseStamped, self.get_pos1, queue_size = 1)
-    self.position_sub2 = rospy.Subscriber('/uav2/sensors/gps', PoseStamped, self.get_pos2, queue_size = 1)
-    self.position_sub3 = rospy.Subscriber('/uav3/sensors/gps', PoseStamped, self.get_pos3, queue_size = 1)
+    self.position_sub = rospy.Subscriber(self.uavName + '/sensors/gps', PoseStamped, self.get_pos, queue_size = 1)
 
-    self.away_velocity_pub1 = rospy.Publisher('/uav1/input/unverified_away_velocity', Vector3, queue_size=1)
-    self.away_velocity_pub2 = rospy.Publisher('/uav2/input/unverified_away_velocity', Vector3, queue_size=1)
-    self.away_velocity_pub3 = rospy.Publisher('/uav3/input/unverified_away_velocity', Vector3, queue_size=1)
+    self.swarm_position_sub = rospy.Subscriber('/swarm/gps', swarm_gps, self.get_swarm_pos, queue_size = 1)
 
-    self.sep_v_w = rospy.get_param("/separation_node/sep_v_w", 0.5)
+    self.uav_avoid = rospy.Subscriber(self.uavName + "/intersection", String, self.get_avoid_name, queue_size = 1)
+
+    self.away_velocity_pub = rospy.Publisher(self.uavName + '/input/unverified_away_velocity', Vector3, queue_size=1)
+
+    self.sep_v_w = rospy.get_param(str(rospy.get_name()) + "/sep_v_w", 0.5)
     print("weight of the 'separation' vector is: " + str(self.sep_v_w))
 
     # Call the mainloop of our class
@@ -35,20 +41,16 @@ class SwarmSeparation():
 
 
   # Callbacks
-  def get_pos1(self, msg):
-    self.curr_pos1.pose.position.x = msg.pose.position.x
-    self.curr_pos1.pose.position.y = msg.pose.position.y
-    self.curr_pos1.pose.position.z = msg.pose.position.z
+  def get_pos(self, msg):
+    self.curr_pos.pose.position.x = msg.pose.position.x
+    self.curr_pos.pose.position.y = msg.pose.position.y
+    self.curr_pos.pose.position.z = msg.pose.position.z
 
-  def get_pos2(self, msg):
-    self.curr_pos2.pose.position.x = msg.pose.position.x
-    self.curr_pos2.pose.position.y = msg.pose.position.y
-    self.curr_pos2.pose.position.z = msg.pose.position.z
+  def get_swarm_pos(self, msg):
+    self.swarm_location = copy.deepcopy(msg)
 
-  def get_pos3(self, msg):
-    self.curr_pos3.pose.position.x = msg.pose.position.x
-    self.curr_pos3.pose.position.y = msg.pose.position.y
-    self.curr_pos3.pose.position.z = msg.pose.position.z
+  def get_avoid_name(self, msg):
+    self.uav_intersection_name = copy.deepcopy(msg)
 
 
   # Main Loop
@@ -59,48 +61,17 @@ class SwarmSeparation():
 
     # While ROS is still running
     while not rospy.is_shutdown():
-      
-      away_2_1_x = 1 / (self.curr_pos2.pose.position.x - self.curr_pos1.pose.position.x)
-      away_2_1_y = 1 / (self.curr_pos2.pose.position.y - self.curr_pos1.pose.position.y)
-      away_2_1_z = 1 / (self.curr_pos2.pose.position.z - self.curr_pos1.pose.position.z)
 
-      away_1_2_x = -1 * away_2_1_x
-      away_1_2_y = -1 * away_2_1_y
-      away_1_2_z = -1 * away_2_1_z
+      if self.swarm_location.name == self.uav_intersection_name:
+        away_x = 1 / (self.curr_pos.pose.position.x - self.swarm_location.x)
+        away_y = 1 / (self.curr_pos.pose.position.y - self.swarm_location.y)
+        away_z = 1 / (self.curr_pos.pose.position.z - self.swarm_location.z)
 
-      away_3_1_x = 1 / (self.curr_pos3.pose.position.x - self.curr_pos1.pose.position.x)
-      away_3_1_y = 1 / (self.curr_pos3.pose.position.y - self.curr_pos1.pose.position.y)
-      away_3_1_z = 1 / (self.curr_pos3.pose.position.z - self.curr_pos1.pose.position.z)
+        self.away_vel.x = away_x * self.sep_v_w
+        self.away_vel.y = away_y * self.sep_v_w
+        self.away_vel.z = away_z * self.sep_v_w
 
-      away_1_3_x = -1 * away_3_1_x
-      away_1_3_y = -1 * away_3_1_y
-      away_1_3_z = -1 * away_3_1_z
-
-      away_3_2_x = 1 / (self.curr_pos3.pose.position.x - self.curr_pos2.pose.position.x)
-      away_3_2_y = 1 / (self.curr_pos3.pose.position.y - self.curr_pos2.pose.position.y)
-      away_3_2_z = 1 / (self.curr_pos3.pose.position.z - self.curr_pos2.pose.position.z)
-
-      away_2_3_x = -1 * away_3_2_x
-      away_2_3_y = -1 * away_3_2_y
-      away_2_3_z = -1 * away_3_2_z
-
-
-      self.away_vel1.x = (away_1_2_x + away_1_3_x) * self.sep_v_w
-      self.away_vel1.y = (away_1_2_y + away_1_3_y) * self.sep_v_w
-      self.away_vel1.z = (away_1_2_z + away_1_3_z) * self.sep_v_w
-      
-      self.away_vel2.x = (away_2_1_x + away_2_3_x) * self.sep_v_w
-      self.away_vel2.y = (away_2_1_y + away_2_3_y) * self.sep_v_w
-      self.away_vel2.z = (away_2_1_z + away_2_3_z) * self.sep_v_w
-      
-      self.away_vel3.x = (away_3_1_x + away_3_2_x) * self.sep_v_w
-      self.away_vel3.y = (away_3_1_y + away_3_2_y) * self.sep_v_w
-      self.away_vel3.z = (away_3_1_z + away_3_2_z) * self.sep_v_w
-
-
-      self.away_velocity_pub1.publish(self.away_vel1)
-      self.away_velocity_pub2.publish(self.away_vel2)
-      self.away_velocity_pub3.publish(self.away_vel3)
+        self.away_velocity_pub.publish(self.away_vel)
 
       # Sleep for the remainder of the loop
       rate.sleep()
