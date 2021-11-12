6ae367aac760eaa4c30a57efb1f706a8b14ed7b4
Avaneen Pinninti <avaneen@gmail.com>
2021-11-12 13:01:43 -0500

    ready to test boid forces - view notes on paper

--- src/mission_waypoints/src/test_files/conversions.py
+++ src/mission_waypoints/src/test_files/conversions.py
@@ -77,9 +77,9 @@ class Conversions():
       self.uav_vel.pe = self.waypoints.pos.y
       self.uav_vel.pd = self.waypoints.pos.z * -1
 
-      self.uav_vel.vn = self.uav_vel.vn / 5
-      self.uav_vel.ve = self.uav_vel.ve / 5
-      self.uav_vel.vd = self.uav_vel.vd / 5
+      # self.uav_vel.vn = self.uav_vel.vn / 5
+      # self.uav_vel.ve = self.uav_vel.ve / 5
+      # self.uav_vel.vd = self.uav_vel.vd / 5
 
       self.vel_pub.publish(self.uav_vel)
 
